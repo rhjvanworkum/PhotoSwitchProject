fb9065054c09eba60550054f9f4164c307b5efdd
sdf <rhjvanworkum@gmail.com>
2021-11-26 11:22:27 +0100

    added utils

---
+++ utils.py
@@ -0,0 +1,67 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+import pandas as pd
+
+def load_property_data(task):
+    """
+    Load data corresponding to the property prediction task.
+    :return: property_vals
+    """
+    df = pd.read_csv('./raw_data/photoswitches.csv')
+    smiles_list = df['SMILES'].to_numpy()
+    
+    if task == 'thermal':
+        # Load the SMILES as x-values and the rate of thermal isomerisation as the y-values
+        property_vals = df['rate of thermal isomerisation from Z-E in s-1'].to_numpy()
+    elif task == 'e_iso_pi':
+        #     Load the SMILES as x-values and the E isomer pi-pi* wavelength in nm as the y-values.
+        #     108 molecules with valid experimental values as of 11 May 2020.
+        property_vals = df['E isomer pi-pi* wavelength in nm'].to_numpy()
+    elif task == 'z_iso_pi':
+        #     Load the SMILES as x-values and the Z isomer pi-pi* wavelength in nm as the y-values.
+        #     84 valid molecules for this property as of 11 May 2020.
+        property_vals = df['Z isomer pi-pi* wavelength in nm'].to_numpy()
+    elif task == 'e_iso_n':
+        #     Load the SMILES as x-values and the E isomer n-pi* wavelength in nm as the y-values.
+        #     96 valid molecules for this property as of 9 May 2020.
+        property_vals = df['E isomer n-pi* wavelength in nm'].to_numpy()
+    elif task == 'z_iso_n':
+        #     Load the SMILES as x-values and the Z isomer n-pi* wavelength in nm as the y-values.
+        #     93 valid molecules with this property as of 9 May 2020
+        #     114 valid molecules with this property as of 16 May 2020
+        property_vals = df['Z isomer n-pi* wavelength in nm'].to_numpy()
+    else:
+        raise Exception('Must specify a valid task')
+
+    return smiles_list, property_vals
+  
+def transform_data(X_train, X_test, y_train, y_test):
+    x_scaler = StandardScaler()
+    X_train_scaled = x_scaler.fit_transform(X_train)
+    X_test_scaled = x_scaler.transform(X_test)
+    y_scaler = StandardScaler()
+    y_train_scaled = y_scaler.fit_transform(y_train)
+    y_test_scaled = y_scaler.transform(y_test)
+
+    return X_train_scaled, X_test_scaled, x_scaler, y_train_scaled, y_test_scaled, y_scaler
+  
+def split_and_rescale_data(X, y, split):
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split, random_state=1)
+  y_train = y_train.reshape(-1, 1)
+  y_test = y_test.reshape(-1, 1)
+  return transform_data(X_train, X_test, y_train, y_test)
+  
+def load_features_and_labels(feature_file, task):
+  smiles_list, property_vals = load_property_data(task)
+  
+  invalid_indices = np.argwhere(np.isnan(property_vals))
+  
+  smiles_list = np.delete(np.array(smiles_list), invalid_indices)
+  property_vals = np.delete(property_vals, invalid_indices)
+  
+  features = pd.read_csv(feature_file).to_numpy()[:, 2:] # remove index and smiles string
+  features = np.delete(features, invalid_indices, axis=0)
+  
+  return features, property_vals
